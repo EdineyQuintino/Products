259ef5f06d0c83557a6d331b4c4008dfce1dea1b
EdineyQuintino <ediney56@outlook.com>
2022-11-09 09:50:19 -0300

    update routes and server

--- src/server.js
+++ src/server.js
@@ -2,14 +2,17 @@ require('dotenv').config();
 const express = require('express');
 
 const userRoutes = require('./routes/user');
+const produtcsRoutes = require('./routes/products');
+const categoriesRoutes = require('./routes/categories');
 
 const app = express();
 
 const PORT = process.env.PORT || 3334;
-
 app.use(express.json());
 
 app.use(userRoutes);
+app.use(produtcsRoutes);
+app.use(categoriesRoutes);
 
 app.listen(PORT,()=>{
     console.log('Running on port',PORT);
