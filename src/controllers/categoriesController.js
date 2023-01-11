82a8d62d756760e5a5db7323c0ccee24a08d6feb
Ediney Oliveira <ediney56@outlook.com>
2023-01-11 13:38:47 -0300

    update on categoriesController

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -3,9 +3,9 @@ const connection = require("../database/prismaClient");
 module.exports = {
   async index(req, resp) {
     try {
-      const categories = await connection.products.findMany();
+      const categories = await connection.categories.findMany();
 
-      return resp.status(200).json(products);
+      return resp.status(200).json(categories);
     } catch (error) {
       return resp.status(400).json({ message: error.message });
     }
@@ -13,13 +13,13 @@ module.exports = {
   async create(req, resp) {
     try {
       const { product_name, productCategory } = req.body;
-      const products = await connection.products.create({
+      const categories = await connection.categories.create({
         data: {
           product_name,
           productCategory,
         },
       });
-      return response.status(201).json({ products });
+      return response.status(201).json({ categories });
     } catch (error) {
       return resp.status(400).json({ message: error.message });
     }
